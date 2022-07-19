872f27d6b92e51ba99439a59b41843bdce2a2924
Antonio <tonino.nacho@gmail.com>
2022-07-18 23:48:22 -0400

    Agregado de un Collapse Nav Bar

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -7,8 +7,11 @@ class Menu extends React.Component{
             <div>
                <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container-fluid">
-                        <div  id="navbarNavDropdown">
-                            <ul class="navbar-nav">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="/">Inicio</a>
                                 </li>
